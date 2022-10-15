dae05c91814cc37dbe4f6024dd5e5002348ff64b
Juanperezaa <sebas8041@hotmail.com>
2022-10-14 19:01:08 -0500

    Camera permissions added

--- app/src/main/java/com/example/buho/AssistanceFragment.kt
+++ app/src/main/java/com/example/buho/AssistanceFragment.kt
@@ -6,9 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.buho.databinding.FragmentAssistanceBinding
+import java.util.jar.Manifest
 
 class AssistanceFragment: Fragment() {
-    private lateinit var _binding: FragmentAssistanceBinding
+    private var _binding: FragmentAssistanceBinding?=null
     private val binding get()=_binding!!
 
     override fun onCreateView(
@@ -18,15 +19,21 @@ class AssistanceFragment: Fragment() {
         // Inflate the layout for this fragment
         _binding=FragmentAssistanceBinding.inflate(inflater,container, false)
         val view= binding.root;
+
+        requestPermissions(arrayOf(
+            android.Manifest.permission.CAMERA,
+        ),1)
         return view;
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        _binding = null
     }
 
     companion object {
         @JvmStatic
         fun newInstance()= AssistanceFragment()
     }
+
 }
