481d3c16336afe283dbe8dc241a0305112646d60
JoseA <josealejandro.garcia@hotmail.com>
2022-10-17 12:07:02 -0500

    added basic profile activity layout

---
+++ app/src/main/java/com/example/buho/ProfileActivity.kt
@@ -0,0 +1,15 @@
+package com.example.buho
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.buho.databinding.ActivityProfileBinding
+
+class ProfileActivity : AppCompatActivity() {
+    private val binding:ActivityProfileBinding by lazy{
+        ActivityProfileBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
