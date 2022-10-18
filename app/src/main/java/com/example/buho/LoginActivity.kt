c35f004af4d6f17438c4a51628d8ef8c059764c4
zacwastaken <doicirohangnan@gmail.com>
2022-10-18 09:02:47 -0500

    made login main activity and cleanup excess activities

--- app/src/main/java/com/example/buho/LoginActivity.kt
+++ app/src/main/java/com/example/buho/LoginActivity.kt
@@ -3,6 +3,8 @@ package com.example.buho
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.buho.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -11,15 +13,35 @@ class LoginActivity : AppCompatActivity() {
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContentView(binding.root)
+
         binding.loginBtn.setOnClickListener{
             val username = binding.usernameET.text.toString()
             val pass = binding.passET.text.toString()
 
-            if(username==R.string.LA_dummy_un.toString()&&pass==R.string.LA_dummy_pass.toString()){
+            val usernameAndPassEmpty = "Mi loco, complicado iniciar sesión si no metés datos"
+            val usernameEmpty = "¿Quién chota sos?"
+            val passwordEmpty = "¿Y la contraseña?"
+            val wrongFields = "Te confundiste de personalidad o esa no es la contraseña"
+
+            if(username == getString(R.string.LA_dummy_un) && pass == getString(R.string.LA_dummy_pass)){
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
+            } else {
+                val toastMsg = when {
+                    username.isEmpty() -> when {
+                        pass.isEmpty() -> usernameAndPassEmpty
+                        else -> usernameEmpty
+                    }
+                    pass.isEmpty() -> passwordEmpty
+                    else -> wrongFields
+                }
+                val toast = Toast.makeText(applicationContext, toastMsg, Toast.LENGTH_LONG)
+
+                toast.show()
             }
         }
+
     }
 }
