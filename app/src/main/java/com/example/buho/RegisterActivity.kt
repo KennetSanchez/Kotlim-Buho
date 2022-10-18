4be4edba8ad7f894ac17994963108233737c153a
Kennet Santiago Sánchez Roldán <1006229463@u.icesi.edu.co>
2022-10-18 18:21:38 -0500

    Register bugged buttons fixed

--- app/src/main/java/com/example/buho/RegisterActivity.kt
+++ app/src/main/java/com/example/buho/RegisterActivity.kt
@@ -12,19 +12,18 @@ class RegisterActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        setContentView(R.layout.activity_register)
+        setContentView(binding.root)
 
         binding.backButton.setOnClickListener{
-            super.finishActivity(0)
+            super.finish()
         }
 
         binding.RARegisterBtn.setOnClickListener{
-            super.finishActivity(1)
             Toast.makeText(applicationContext, "Usuario registrado exitosamente", Toast.LENGTH_LONG).show()
             super.finish()
-            binding.RARegisterBtn.setBackgroundColor(R.color.btn_danger)
         }
 
 
