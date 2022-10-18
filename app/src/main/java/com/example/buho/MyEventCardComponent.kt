ccde653125ff18294ad698a37f306157beff8568
zacwastaken <doicirohangnan@gmail.com>
2022-10-18 17:53:10 -0500

    recycler view modifiable

--- app/src/main/java/com/example/buho/MyEventCardComponent.kt
+++ app/src/main/java/com/example/buho/MyEventCardComponent.kt
@@ -1,14 +1,9 @@
 package com.example.buho
 
-import android.annotation.SuppressLint
-import android.content.Context
-
-@SuppressLint("ViewConstructor")
 class MyEventCardComponent (
     val title: String,
     val state: String,
     val classroom: String,
     val schedule: String,
-    val description: String,
-    context: Context,
+    val description: String
 ) {}
