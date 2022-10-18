bf1293a3885e8b989a5c3470de46e0b60f164cfb
Kennet Santiago Sánchez Roldán <1006229463@u.icesi.edu.co>
2022-10-18 17:41:43 -0500

    Register layout updated, login buttons reorganized

--- app/src/main/java/com/example/buho/RegisterActivity.kt
+++ app/src/main/java/com/example/buho/RegisterActivity.kt
@@ -2,17 +2,31 @@ package com.example.buho
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.buho.databinding.ActivityMainBinding
+import android.widget.Toast
+import com.example.buho.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
 
-    private val binding: ActivityMainBinding by lazy{
-        ActivityMainBinding.inflate(layoutInflater)
+    private val binding: ActivityRegisterBinding by lazy{
+        ActivityRegisterBinding.inflate(layoutInflater)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         setContentView(R.layout.activity_register)
+
+        binding.backButton.setOnClickListener{
+            super.finishActivity(0)
+        }
+
+        binding.RARegisterBtn.setOnClickListener{
+            super.finishActivity(1)
+            Toast.makeText(applicationContext, "Usuario registrado exitosamente", Toast.LENGTH_LONG).show()
+            super.finish()
+            binding.RARegisterBtn.setBackgroundColor(R.color.btn_danger)
+        }
+
+
     }
 }
