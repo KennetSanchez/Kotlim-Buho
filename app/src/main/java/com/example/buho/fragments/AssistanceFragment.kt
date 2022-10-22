715c4214cd6afadd7e11480f688aa5536dc8720e
Kennet Santiago Sánchez Roldán <1006229463@u.icesi.edu.co>
2022-10-22 00:52:52 -0500

    Readme added, camera permissions are requested now
    The application it's able to ask for the camera permissions the first time the user tries to scan a qr code.

--- app/src/main/java/com/example/buho/fragments/AssistanceFragment.kt
+++ app/src/main/java/com/example/buho/fragments/AssistanceFragment.kt
@@ -1,14 +1,18 @@
 package com.example.buho.fragments
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.buho.R
 import com.example.buho.databinding.AssistancePageBinding
@@ -25,10 +29,20 @@ class AssistanceFragment : Fragment(R.layout.assistance_page) {
         _binding= AssistancePageBinding.inflate(inflater,container, false)
         val view= binding.root;
         binding.qrScanBtn.setOnClickListener{
-            startForResult.launch(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
+            requestPermissionLauncher.launch(Manifest.permission.CAMERA)
         }
 
         return view;
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            startForResult.launch(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
+        } else {
+            Toast.makeText(context, "Se necesita permiso para usar la cámara", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private val startForResult=registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
